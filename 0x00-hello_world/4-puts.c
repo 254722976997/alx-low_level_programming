453ddb4f77e6aadaf52510015f58998c9a82e0a2
Martin Mwangi <martin.hgvdriver@gmail.com>
2023-08-14 10:47:07 -0700

    C Practice

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: using the puts funtction
+ * to print a block of text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

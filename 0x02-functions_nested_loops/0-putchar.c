510e7ef1fc83ce738a514f62ebfdaa33ec708e69
254722976997 <martin.hgvdriver@gmail.com>
2023-08-16 02:13:17 +0300

    0_putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints _putchar
+ *
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

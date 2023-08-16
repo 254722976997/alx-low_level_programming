60a779a5279b44a8b175ecb551b44d26c1896100
Martin Mwangi <martin.hgvdriver@gmail.com>
2023-08-16 10:37:54 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descriotion: A program that prints out sing digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}

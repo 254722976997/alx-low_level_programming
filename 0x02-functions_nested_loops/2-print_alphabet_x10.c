56db7830ac60735c3ae068bb1a8cde69f78dd4c5
Martin Mwangi <martin.hgvdriver@gmail.com>
2023-08-16 11:20:58 -0700

    GC manenos

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+**print_alphabet_x10 - Entry point
+*Prints 10 times the alphabet in lowercase.
+* Return: Always 0.
+*/
+void print_alphabet_x10(void)
+{
+int k;
+int j;
+for (j = 0; j < 10; j++)
+{
+	for (k = 97; k < 123; k++)
+		_putchar(k);
+	_putchar('\n');
+}
+}

6f1f77b5a2188c7c7dd3ea6d988ba1932d71b247
Martin Mwangi <martin.hgvdriver@gmail.com>
2023-08-18 13:36:26 -0700

    We are getting there

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main-entry point
+ * Description : a program that checks percae
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}

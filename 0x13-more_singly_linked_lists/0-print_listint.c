188f992c88a033ec8fad73ef2e616730f6498929
Martin Mwangi <martin.hgvdriver@gmail.com>
2023-09-25 23:04:29 +0300

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
-* print_listint - prints all elements of list.
+* print_listint - prints all elements of list
 *
-* @h: list to print elements of.
+* @h: list to print elements of
 * Return: size_t of list
-*/
+	    */
 size_t print_listint(const listint_t *h)
 {
 	size_t n = 0;
